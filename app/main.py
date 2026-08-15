dfbaec7d9b76badcaf1b284ce12abcdfd2a4ba09
andriynavr07 <navrand007@gmail.com>
2026-08-15 17:22:17 +0300

    Success

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 class Person:
-    # write your code here
-    pass
+    people: dict = {}
 
+    def __init__(self, name : str, age : int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    person_instances = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
+    for person_data, person in zip(people, person_instances):
+        if person_data.get("wife"):
+            person.wife = Person.people[person_data["wife"]]
+        elif person_data.get("husband"):
+            person.husband = Person.people[person_data["husband"]]
+
+    return person_instances
